d9e760b585c871596e3fd8e93c0a414e6b7dbdb8
Kevin Chou <mrguin33@gmail.com>
2022-01-12 10:53:52 +0800

    Some cv wakeup logic improvements: remove unnecessary cv.notify

--- lock-queue-I/main.cpp
+++ lock-queue-I/main.cpp
@@ -13,40 +13,30 @@ const int ROUNDS = 20;
 const int DONE = -1;
 
 // invariant:
-// either next_candy < CANDY_NUM && can_grab = true
-// or next_candy == CANDY_NUM && can_grab = false
-// or next_candy == DONE && can_grab = true (which means over)
+// either next_candy < CANDY_NUM && can_grab == true
+// or next_candy == CANDY_NUM && can_grab == false
+// or next_candy == DONE && can_grab == true (which means over)
 int next_candy = CANDY_NUM;
 bool can_grab = false;
 
 std::mutex candy_mutex;
-std::condition_variable cv_worker;
-std::condition_variable cv_main;
-// Distinguish cv_main from cv_worker to avoid unnecessary wakeup.
-// Thus we can use cv_worker.notify_one() to wakeup exactly
-// one worker after current worker just finished grabbing.
-// Otherwise we have to use cv.notify_all() to wakeup both all
-// workers and main every time.
+std::condition_variable cv;
 
 void grab_candy() {
     while (true) {
-        std::unique_lock <std::mutex> lk(candy_mutex);
-        cv_worker.wait(lk, [] { return can_grab; });
+        std::unique_lock <std::mutex> lk(candy_mutex);  // workers blocked here trying to grab the lock
+        cv.wait(lk, [] { return can_grab; });    // workers blocked here waiting for a new round
         if (next_candy >= 0 && next_candy < CANDY_NUM) {
             std::cout << "Thread " << std::this_thread::get_id()
                       << " grabs candy " << next_candy << std::endl;
             next_candy++;
-            lk.unlock();
-            cv_worker.notify_one();    // release lock, notify one worker thread
         } else if (next_candy == CANDY_NUM) {
             can_grab = false;
-            lk.unlock();
-            cv_main.notify_one();   // notify main
+            cv.notify_all();   // notify main
         } else {
             std::cout << "All rounds finished, thread " << std::this_thread::get_id()
                       << " exiting..." << std::endl;
-            cv_worker.notify_one();    // release lock, notify one worker thread
-            return;
+            break;
         }
     }
 }
@@ -58,21 +48,19 @@ int main() {
     }
     for (int i = 0; i <= ROUNDS; i++) {
         std::unique_lock <std::mutex> lk(candy_mutex);
-        cv_main.wait(lk, [] { return !can_grab; });
+        cv.wait(lk, [] { return !can_grab; });
         std::cout << "Candies all grabbed, main thread wakes up." << std::endl;
         if (i == ROUNDS) {  // finished
             next_candy = DONE;
             can_grab = true;
-            lk.unlock();
-            cv_worker.notify_one();    // notify one worker thread
+            cv.notify_all();    // notify all workers
             std::cout << "All rounds finished, waiting for all threads exit..." << std::endl;
             break;
         }
         next_candy = 0;
         can_grab = true;
         std::cout << "Main thread starting a new round: " << i << std::endl;
-        lk.unlock();
-        cv_worker.notify_one();     // notify one worker thread
+        cv.notify_all();     // notify all workers
     }
     for (auto i = 0; i < THREAD_NUM; i++) {
         threads[i].join();
